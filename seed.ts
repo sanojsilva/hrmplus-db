02893d297734cc4531ae1b4c7e3b75bf064431b6
Sachini Samaraweera <sachipam@gmail.com>
2021-09-12 09:46:04 +0530

    Add Leave and Recruitment Permissions

--- seed.ts
+++ seed.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import authPermissions from "./seeds/permissions/auth";
+import leavePermissions from "./seeds/permissions/leave";
 import lifecyclePermissions from "./seeds/permissions/lifecycle";
+import recruitmentPermissions from "./seeds/permissions/recruitment";
 import roles from "./seeds/roles";
 const db = new PrismaClient();
 
 async function main() {
+  //1-100
   for (const permission of authPermissions) {
     await db.permission.upsert({
       where: {
@@ -20,7 +23,40 @@ async function main() {
     });
   }
 
+  //100-200
   for (const permission of lifecyclePermissions) {
+    await db.permission.upsert({
+      where: {
+        id: permission.id,
+      },
+      update: {
+        ...permission,
+      },
+      //@ts-ignore
+      create: {
+        ...permission,
+      },
+    });
+  }
+
+  //300-400
+  for (const permission of recruitmentPermissions) {
+    await db.permission.upsert({
+      where: {
+        id: permission.id,
+      },
+      update: {
+        ...permission,
+      },
+      //@ts-ignore
+      create: {
+        ...permission,
+      },
+    });
+  }
+
+  //400-500
+  for (const permission of leavePermissions) {
     await db.permission.upsert({
       where: {
         id: permission.id,
