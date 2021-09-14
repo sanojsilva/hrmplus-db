dff98707f1522f7ac1c8e6c08dd520fb6a672dce
Sachini Samaraweera <sachipam@gmail.com>
2021-09-14 09:34:16 +0530

    update fields and seed files

---
+++ seeds/grade.ts
@@ -0,0 +1,34 @@
+import { Grade } from "@prisma/client";
+
+const grade: Partial<Grade>[] = [
+  {
+    id: 1,
+    name: "G1",
+  },
+  {
+    id: 2,
+    name: "G2",
+  },
+  {
+    id: 3,
+    name: "G3",
+  },
+  {
+    id: 4,
+    name: "G4",
+  },
+  {
+    id: 5,
+    name: "G5",
+  },
+  {
+    id: 6,
+    name: "G6",
+  },
+  {
+    id: 7,
+    name: "G7",
+  },
+];
+
+export default grade;
